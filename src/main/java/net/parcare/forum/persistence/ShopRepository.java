0b5f8e129972d2c763892ba4bec3e1893664de81
manhorro <manhorro@yahoo.com>
2018-11-21 12:21:51 +0200

    mutare la mine

---
+++ src/main/java/net/parcare/forum/persistence/ShopRepository.java
@@ -0,0 +1,11 @@
+package main.java.net.parcare.forum.persistence;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ShopRepository
+        extends
+        PagingAndSortingRepository<Shop, Long> {
+
+
+
+}
