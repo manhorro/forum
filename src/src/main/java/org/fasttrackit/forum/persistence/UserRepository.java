1297db75f04cef57a434f15e8f8c56f2f17aa540
manhorro <manhorro@yahoo.com>
2018-11-22 10:28:35 +0200

    module

--- src/src/main/java/org/fasttrackit/forum/persistence/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.fasttrackit.forum.persistence;
-
-
-import main.java.net.parcare.forum.domain.User;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface UserRepository extends PagingAndSortingRepository<User, Long> {
-}
